a6eb3765e0a6bcf13245de6da1bb4eea072642aa
karlley <karlley.jp@gmail.com>
2020-03-21 05:24:10 +0900

    add route POST values#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'values/new'
-  get 'values/show'
-  get 'values/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/values/new', to: 'values#new'
+  get '/values/show', to: 'values#show'
+  get '/values/index', to: 'values#index'
+  post '/values', to: 'values#create'
 end
